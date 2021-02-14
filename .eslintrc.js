7c8cf0174c59339607063882b7027fe1bff779b5
tanliang <244739228@qq.com>
2021-02-14 23:08:10 +0800

    feat: 配置好了eslint和prettier规则 开始写代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,17 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/prettier/@typescript-eslint",
+    "@vue/prettier"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: 0,
+    "no-empty": 2, //块语句中的内容不能为空
+    "no-var": 0 //禁用var，用let和const代替
   }
-};
+}
